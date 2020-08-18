f0dc52bb9837128e6f773f2ccdf8dd2c45c400e2
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-08-18 19:08:16 -0300

    Task 1 - Done

---
+++ exercises/8_2/aula.js
@@ -0,0 +1,18 @@
+const numeros = [1, 2, 3, 4, 5, 6];
+console.log(numeros.map(numero => numero * 2));
+// Retorno: [2, 4, 6, 8, 10, 12]
+
+const paresMenoresQueCinco = [];
+numeros.forEach(numero => {
+  if (numero < 5 && numero % 2 === 0) {
+    paresMenoresQueCinco.push(numero);
+  }
+})
+console.log(paresMenoresQueCinco); // Retorno: [2, 4]
+
+// ------------------------------------------------/
+
+const allNameStudents = estudantes.filter((estudante) => (
+  estudante.turno === 'Manhã')).map((estudante) => `${estudante.nome} ${estudante.sobrenome}`);
+
+console.log(allNameStudents);
