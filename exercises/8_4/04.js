a4fb2e43528a6e2d871cf07acae59d3b8c061373
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-08-21 14:58:57 -0300

    Task 4 - Done

--- exercises/8_4/04.js
+++ exercises/8_4/04.js
@@ -1,1 +1,46 @@
+//Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
 
+// Dica: use object destructuring.
+
+const assert = require('assert')
+
+const people = [{
+    name: "Nicole",
+    bornIn: 1992,
+    nationality: "Australian"
+  },
+  {
+    name: "Harry",
+    bornIn: 2008,
+    nationality: "Australian"
+  },
+  {
+    name: "Toby",
+    bornIn: 1901,
+    nationality: "Australian"
+  },
+  {
+    name: "Frida",
+    bornIn: 1960,
+    nationality: "Dannish"
+  },
+  {
+    name: "Fernando",
+    bornIn: 2001,
+    nationality: "Brazilian"
+  }
+]
+
+const filterPeople = (people) => {
+  return people.filter(person => {
+    const { name, bornIn, nationality } = person;
+    return ((nationality === "Australian") && (bornIn <= 2000 && bornIn > 1900));
+  });
+};
+
+const filteredPeople = filterPeople(people)
+
+console.log(filteredPeople[0]);
+
+assert.deepEqual(filteredPeople[0], { name: "Nicole", bornIn: 1992, nationality: "Australian" })
+assert.deepEqual(filteredPeople[1], { name: "Toby", bornIn: 1901, nationality: "Australian" })
