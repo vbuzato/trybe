d488ad20b6630e86ce5d8765abc492107c5d15ff
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-09-03 23:32:28 -0300

    Exercise 3 - Done

--- exercises/10_2/02.test.js
+++ exercises/10_2/02.test.js
@@ -4,6 +4,6 @@ it('Check whether getUserName function returns an existent name', () => {
   return expect(getUserName(4)).resolves.toBe('Mark');
 })
 
-it('Check getUserName function returns an existent name', () => {
+it('Check whether getUserName function returns an error when id isn`t found', () => {
   return expect(getUserName(6)).rejects.toEqual({ error: 'User with 6 not found.' })
 })
