0132042a34554cbdd15cb39b38014b00e11f1ee9
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-09-15 23:28:59 -0300

    Exercise 1 - Done

--- exercises/12_1/my-pokedex/src/pokedex.js
+++ exercises/12_1/my-pokedex/src/pokedex.js
@@ -3,15 +3,33 @@ import Pokemon from './pokemon'
 import pokemons from './data'
 
 class Pokedex extends Component {
+
+  constructor() {
+    super();
+
+    this.nextPokemon = this.nextPokemon.bind(this)
+
+    this.state = {
+      index: 0
+    }
+  }
+
+  nextPokemon() {
+    this.setState((previousState) => ({
+      index: (previousState.index < pokemons.length - 1) ? previousState.index + 1 : 0
+    }));
+  }
+
   render() {
     return (
       <div className='pokedex-list'>
-      {/* {this.props.pokemons.map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon} /> )} */}
-      <Pokemon key={pokemons[0].id} pokemon={pokemons[0]} />
-    </div>
+        <Pokemon key={pokemons[this.state.index].id} pokemon={pokemons[this.state.index]} />
+        <button onClick={this.nextPokemon}>next</button>
+      </div>
     );
   }
 }
 
 export default Pokedex;
 
+{/* {this.props.pokemons.map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon} /> )} */}
