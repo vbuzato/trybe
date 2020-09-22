36822765c6563c53ba8cd34ff37ce231ada01e15
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-09-21 22:53:44 -0300

    Exercise 2 - Done

--- exercises/13_1/exercise-dog-image/src/Dogs.js
+++ exercises/13_1/exercise-dog-image/src/Dogs.js
@@ -5,26 +5,42 @@ class Dogs extends React.Component {
     super();
     this.fetchImageDogs = this.fetchImageDogs.bind(this);
     this.state = {
-      dogImagePath: undefined
+      dogImagePath: undefined,
+      breed: undefined
     }
+  }
+
+  capitalizeName(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1);
   }
   
   async fetchImageDogs() {
     const fetchData = await fetch('https://dog.ceo/api/breeds/image/random');
     const imagePath = await fetchData.json();
-    this.setState({ dogImagePath: imagePath.message });
+    const erro = 'https://www.dronekids.com.br/wp-content/uploads/2018/08/proibido-importar1.jpg';
+    const dogBreed = imagePath.message.split('/')[4];
+    this.setState({ 
+      dogImagePath: (!imagePath.message.includes('terrier')) ? imagePath.message : erro,
+      breed: this.capitalizeName(dogBreed)
+    });
   }
   
   componentDidMount() {
     this.fetchImageDogs()
   }
+
+  componentDidUpdate() {
+    localStorage.setItem('lastURL', this.state.dogImagePath);
+  }
+
   render() {
-    const { dogImagePath } = this.state;
+    const { dogImagePath, breed } = this.state;
     return (
-      <div>
-        <button type="button" onClick={this.fetchImageDogs}>Trocar de foto</button><br />
+      <article className="dogInfo">
+        <button type="button" onClick={this.fetchImageDogs}>Trocar de foto</button>
+        <span className="breed">Raça: {breed}</span>
         {(dogImagePath) ? <img src={dogImagePath} alt="" width="300px" /> : 'Loading...'}
-      </div>
+      </article>
     );
   }
 }
