924fefeed2850c1c4a49c1e14a5fe66ce67880ab
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-09-21 23:48:33 -0300

    Updates

---
+++ precache-manifest.e1490645c6eccfb9817e5f856f541846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fddcfe48e8ec18b5f7e32aa23c3adf1d",
+    "url": "/projects/dog-image/index.html"
+  },
+  {
+    "revision": "374a699aed85b66c3ba7",
+    "url": "/projects/dog-image/static/css/main.c8d1e04a.chunk.css"
+  },
+  {
+    "revision": "08f8f6cbcb6d3e902d23",
+    "url": "/projects/dog-image/static/js/2.b337f136.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projects/dog-image/static/js/2.b337f136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "374a699aed85b66c3ba7",
+    "url": "/projects/dog-image/static/js/main.7b4bcd28.chunk.js"
+  },
+  {
+    "revision": "7ef770460c973ae3dc67",
+    "url": "/projects/dog-image/static/js/runtime-main.6c595232.js"
+  }
+]);
